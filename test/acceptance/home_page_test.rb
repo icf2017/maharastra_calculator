5db26084b526832a632f650644052e0ad9731ece
ronak <ronakjain90@gmail.com>
2018-03-17 11:04:17 +0530

    feat(unit_tests): Fixed broken tests

--- test/acceptance/home_page_test.rb
+++ test/acceptance/home_page_test.rb
@@ -11,76 +11,76 @@ describe "Home page" do
 
   it 'visits_all_other_pages_to_do_regression_test' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     assert page.find('div#results').text.include?('Energy Demand')
   end
 
   it 'visits_all_energy' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'All Energy'
     assert page.find('div#results').text.include?('Energy Demand')
   end
 
   it 'visits_electricity' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Electricity'
     assert page.find('div#results').text.include?('Electricity demand')
   end
 
   it 'visits_energy_security' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Energy Security'
     assert page.find('div#results').text.include?('Percentage Import Dependence')
   end
 
   it 'visits_energy_emissions' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Energy Emissions'
     assert page.find('div#results').text.include?('Total Emissions')
   end
 
   it 'visits_energy_flows' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Energy Flows'
     assert page.find('div#results').text.include?('Flows in the Indian Energy System')
   end
 
   it 'visits_land_requirements' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Land Requirement'
     assert page.find('div#results').text.include?('Land Footprint')
   end
 
   it 'visits_grid_balancing' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Grid Balancing'
     assert page.find('div#results').text.include?('Grid dispatch pattern')
   end
 
   it 'visits_energy_cost' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Energy Costs'
     assert page.find('div#results').text.include?('energy system costs')
   end
 
   it 'visits_my_story' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'My Story'
     assert page.find('div#results').text.include?('The chosen energy pathway')
   end
 
   it 'visits_assumptions' do
     visit "/"
-    page.find('body .popover .popover-navigation').click_button('Enter Calculator')
+    page.find('body .popover .popover-navigation').click_button('End tour')
     click_on 'Assumptions'
     assert page.find('div#results').text.include?('Assumptions')
   end
