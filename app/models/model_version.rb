68092769e1c04bc79892c65c34c4148f0b9824b8
ronak <ronakjain90@gmail.com>
2018-05-03 13:08:55 +0530

    chore - Final changes and model update

--- app/models/model_version.rb
+++ app/models/model_version.rb
@@ -1,1 +1,1 @@
-def Model.last_modified_date() @last_modified_date ||= Time.utc(*[5, 1, 13, 2, 5, 2018, 3, 122, false, "IST"]); end
+def Model.last_modified_date() @last_modified_date ||= Time.utc(*[42, 22, 12, 3, 5, 2018, 4, 123, false, "IST"]); end
